2e78df48a20a4de8ac1ca1c6009b0ad5ad38fa8a
thoriqmacto <thariq.hadad@gmail.com>
2023-12-12 21:29:37 +0700

    Finish stage-3

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,37 +1,72 @@
 package machine
 
+
 fun main() {
-    val totalCups = readln().toInt()
-    val machine = CoffeeMachine(totalCups)
+    val machine = CoffeeMachine()
+    machine.run()
 }
 
-class CoffeeMachine(val totalCups:Int) {
-    private val waterPerCup = 200
-    private val milkPerCup = 50
-    private val beansPerCup = 15
+class CoffeeMachine {
+    private val waterPerCup = 200.0
+    private val milkPerCup = 50.0
+    private val beansPerCup = 15.0
+    private var waterAvailable = 0.0
+    private var milkAvailable = 0.0
+    private var beansAvailable = 0.0
+    private var numCoffeeAsRequested = 0
+    private var numCoffeeAsPerResource = 0
 
-    val messageStatus = mapOf(
-        1 to "Starting to make a coffee",
-        2 to "Grinding coffee beans",
-        3 to "Boiling water",
-        4 to "Mixing boiled water with crushed coffee beans",
-        5 to "Pouring coffee into the cup",
-        6 to "Pouring some milk into the cup",
-        7 to "Coffee is ready!"
-        )
+//    val messageStatus = mapOf(
+//        1 to "Starting to make a coffee",
+//        2 to "Grinding coffee beans",
+//        3 to "Boiling water",
+//        4 to "Mixing boiled water with crushed coffee beans",
+//        5 to "Pouring coffee into the cup",
+//        6 to "Pouring some milk into the cup",
+//        7 to "Coffee is ready!"
+//        )
 
     init {
-        calculateIngredients()
+        calculateResource()
+
+        println("Write how many cups of coffee you will need:")
+        numCoffeeAsRequested = readln().toInt()
+
+        calculateCoffeeCapacity()
     }
 
-    private fun calculateIngredients(){
-        val waterNeeded = totalCups * waterPerCup
-        val milkNeeded = totalCups * milkPerCup
-        val beansNeeded = totalCups * beansPerCup
+    private fun calculateResource() {
+        println("Write how many ml of water the coffee machine has:")
+        waterAvailable = readln().toDouble()
+        println("Write how many ml of milk the coffee machine has:")
+        milkAvailable = readln().toDouble()
+        println("Write how many grams of coffee beans the coffee machine has:")
+        beansAvailable = readln().toDouble()
 
-        println("For $totalCups cups of coffee you will need:")
-        println("$waterNeeded ml of water")
-        println("$milkNeeded ml of milk")
-        println("$beansNeeded g of coffee beans")
+        val totalCoffeeBasedOnWater = (waterAvailable / waterPerCup).toInt()
+        val totalCoffeeBasedOnMilk = (milkAvailable / milkPerCup).toInt()
+        val totalCoffeeBasedOnBeans = (beansAvailable / beansPerCup).toInt()
+
+        numCoffeeAsPerResource = minOf(totalCoffeeBasedOnWater,
+            totalCoffeeBasedOnMilk,
+            totalCoffeeBasedOnBeans)
+    }
+
+    private fun calculateCoffeeCapacity(){
+        println("Num. Coffee Requested: $numCoffeeAsRequested")
+        println("Num. Coffee Resources: $numCoffeeAsPerResource")
+
+        if(numCoffeeAsRequested > numCoffeeAsPerResource){
+            println("No, I can make only $numCoffeeAsPerResource cups of coffee")
+        }else if(numCoffeeAsRequested == numCoffeeAsPerResource) {
+            println("Yes, I can make that amount of coffee")
+        }else{
+            val surplus = numCoffeeAsPerResource - numCoffeeAsRequested
+            println("Yes I can make that amount of coffee (and even $surplus more than that)")
+        }
+    }
+
+    fun run(){
+        return
     }
 }
